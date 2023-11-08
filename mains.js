7fb518133323f0fa0bc3b9516ee120a36140be9f
bruno-cafeina <135143536+bruno-cafeina@users.noreply.github.com>
2023-11-08 12:13:30 -0300

    Add files via upload

--- mains.js
+++ mains.js
@@ -1,28 +1,38 @@
 function tocaSom(idElementoAudio){
-document.querySelector(idElementoAudio).play();
+  const elemento = document.querySelector(idElementoAudio);
+  if(elemento === null) {
+   alert('Elemento não encontrado');
+  };
+  if (elemento && elemento.localName === 'audio') {
+   elemento.play();
+  }else {
+   alert('Elemento não encontrado');
+  };
 }
-const listaDeTeclas=document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+        for(let contador = 0; contador < listaDeTeclas.length;contador++) {
+            const tecla = listaDeTeclas[contador];
+            const instrumento = tecla.classList[1];
+            const idAudio = `#som_${instrumento}`;
+            console.log(idAudio);
+         
+            tecla.onclick = function(){
+               tocaSom(idAudio);  
+            };
+            tecla.onkeydown = function (evento) {
+               if(evento.code === 'Enter'|| evento.code === 'Space'){
+               tecla.classList.add('ativa');
+            }
+        };
+            tecla.onkeyup = function(){
+                tecla.classList.remove('ativa');
+            };
+            console.log(instrumento);
+         console.log(contador);
+    
+      }
 
-let contador = 0;
-//Estrutura de repetição - Enquanto
- for(contador <listaDeTeclas.length) {
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const iAudio = `#som_${instrumento}`;
-    console.log(iAudio);
-
-    tecla.onclick = function(){
-        tocaSom(iAudio);
-    };
-    tecla.onkeydown = function(evento){
-        console.log(evento.code) ;
-    if (evento.code === 'Enter') 
-     tecla.classList.add('ativa');
-   }
-   if (evento.code === 'Space'){
-     tecla.classList.add('ativa');
-   }
-    tecla.onkeyup = function(){
-        tecla.classList.remove('ativa');
-  }
- }
+    
+    
+   
+     
