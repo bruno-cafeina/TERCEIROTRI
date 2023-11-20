48fd95d8ad90d2ab370bf3f16809951972e370df
bruno-cafeina <135143536+bruno-cafeina@users.noreply.github.com>
2023-11-20 12:29:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+//a função que deve ser executada é tocaSom, mas que som? Nessse caso, será o parâmetro idElementoAldio. Ou seja, o endereço da tecla será procurando no html e reproduzindo.
+function tocaSom(idElementoAudio){
+  const elemento = document.querySelector(idElementoAudio);
+//se o elemento pesquisado for igual aa "inexistente", o alerta de elemento não encontrado aparecerá.
+  if(elemento === null) {
+   alert('Elemento não encontrado');
+  };
+  //se o elemento existir, a função play daquele elemento acontecerá, ou seja, a tecla será reproduzida.
+  if (elemento && elemento.localName === 'audio') {
+   elemento.play();
+  }else {
+   alert('Elemento não encontrado');
+  };
+}
+//querySelector encontra as informações de tecla pesquisada no documento
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//Estrutura de repetição - Enquanto
+//para que as teclas não sejam repetidas de maneira infinita, criamos o for, com valor de  início 0, e valor final chamado de contador < listaDeTeclas.length, para que a repetição seja exatamente do tamanho da nossa lista, que é de 9 teclas.
+        for(let contador = 0; contador < listaDeTeclas.length;contador++) {
+            const tecla = listaDeTeclas[contador];
+// classList armazena todas as informações do elemento pesquisado
+            const instrumento = tecla.classList[1];
+// o idAudio é responsável por identificar o som do botão pesquisado
+            const idAudio = `#som_${instrumento}`;
+
+            console.log(idAudio);
+//onclick significa "ao clicar na teclas" a função tocaSom será executada, o id daquele audio será buscado e reproduzido.
+            tecla.onclick = function(){
+               tocaSom(idAudio);  
+            };
+//onkeydown significa "quando a tecla estiver pressionada", nessse caso uma função será executada, e qual é essa função? É a estilização de mudança de cor do botão quando as teclas enter e Space forem apertadas.
+            tecla.onkeydown = function (evento) {
+               if(evento.code === 'Enter'|| evento.code === 'Space'){
+               tecla.classList.add('ativa');
+            }
+        };
+//onkeyup significa "quando a tecla estiver erguida, não pressionada", nesse caso, a função será removidainiciosole.log(instrumento);
+         console.log(contador);
+    
+      }
+
+    
+    
+   
+     
